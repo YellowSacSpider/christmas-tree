0272a7a78aef157ef6c389c6da0f7ce494b8e93c
YellowSacSpider <teemypl@gmail.com>
2023-03-05 23:36:20 +0100

    Update christmas-tree-5.cpp

--- christmas-tree-5.cpp
+++ christmas-tree-5.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
@@ -14,11 +6,11 @@ int main()
 {
     int n = 0;
     std::cin >> n;
-    for(int i = 0; i < n; i+=2){
-        for(int k = n; k >= i; k-=2){
+    for(int i = 0; i < n; i++){
+        for(int k = n; k >= i; k--){
             std::cout << " ";
         }
-        for(int j = 0; j < i+1; j++){
+        for(int j = 0; j < 2*i+1; j++){
             std::cout << "*";
         }
         std::cout << '\n';
